a8ded4b999134e562c21bf253bd038680ec63f08
Zannu Julius <zannujulius14@gmail.com>
2020-08-04 16:55:43 +0100

    added ejs

--- app.js
+++ app.js
@@ -1,9 +1,10 @@
 const path = require('path')
 const express = require('express')
 const app = express();
+const ejs = require('ejs')
 
 const port = process.env.PORT || 3000;
-
+app.set('view engine', 'ejs')
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/', (req, res) => {
