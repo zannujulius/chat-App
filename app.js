1e4049ac961f075c4c878fbd7046b5c92e7dfcf3
Zannu Julius <zannujulius14@gmail.com>
2020-08-04 21:49:05 +0100

    connect and disconnected

--- app.js
+++ app.js
@@ -1,7 +1,13 @@
 const path = require('path')
 const express = require('express')
-const app = express();
+
 const ejs = require('ejs')
+const socketIO = require('socket.io');
+const http = require('http')
+
+let app = express();
+let server = http.createServer(app)
+let io = socketIO(server);
 
 const port = process.env.PORT || 3000;
 app.set('view engine', 'ejs')
@@ -11,6 +17,25 @@ app.get('/', (req, res) => {
     res.render('index')
 })
 
-app.listen(port, () => {
+// while listen for an event from the client or user
+io.on('connection', socket => {
+    console.log('A new User just connected')
+
+    // when th user is disconnected
+    socket.on('disconnect', () => {
+        console.log('User was disconnected!!')
+    })
+})
+
+
+server.listen(port, () => {
     console.log(`Server is running on port ${port}`)
 })
+
+
+// the client side is going to be making the connect 
+// the server side is going to be taking the cinnection and allowing them
+// socket.io helps handle connectioin very easy
+// socket.io doesnt work well with app.use 
+// it works well with http server protocols
+//
